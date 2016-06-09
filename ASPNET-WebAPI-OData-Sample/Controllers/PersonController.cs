938a73c5e93185f663338aad5727bf8eb60bf20b
Fabian Gosebrink <fabian.gosebrink@hotmail.com>
2016-06-09 17:25:16 +0200

    extended personcontroller

--- ASPNET-WebAPI-OData-Sample/Controllers/PersonController.cs
+++ ASPNET-WebAPI-OData-Sample/Controllers/PersonController.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net;
+using System.Web.Http;
 using System.Web.OData;
 using System.Web.OData.Routing;
 using ASPNET_WebAPI_OData_Sample.DataAccess.Repositories;
+using ASPNET_WebAPI_OData_Sample.Models.Entities;
 
 namespace ASPNET_WebAPI_OData_Sample.Controllers
 {
@@ -35,6 +38,90 @@ namespace ASPNET_WebAPI_OData_Sample.Controllers
             }
 
             return Ok(SingleResult.Create(personEntity));
+        }
+
+        [HttpPost]
+        [ODataRoute("Persons")]
+        public IHttpActionResult CreateNewPerson(PersonEntity personEntity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PersonEntity addedEntity = _personRepository.Add(personEntity);
+            _personRepository.SaveToDb();
+
+            return Created(addedEntity);
+        }
+
+        [HttpPut]
+        [ODataRoute("Persons({id})")]
+        public IHttpActionResult UpdateFullPerson([FromODataUri] int id, PersonEntity personEntity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PersonEntity existingPersonEntity = _personRepository.GetSingle(id).First();
+
+            if (existingPersonEntity == null)
+            {
+                return NotFound();
+            }
+
+            personEntity.Id = existingPersonEntity.Id;
+
+            _personRepository.Update(personEntity);
+            _personRepository.SaveToDb();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPatch]
+        [ODataRoute("Persons({id})")]
+        public IHttpActionResult Patch([FromODataUri] int id, Delta<PersonEntity> personEntity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PersonEntity existingPersonEntity = _personRepository.GetSingle(id).First();
+
+            if (existingPersonEntity == null)
+            {
+                return NotFound();
+            }
+
+            personEntity.Patch(existingPersonEntity);
+            _personRepository.Update(existingPersonEntity);
+            _personRepository.SaveToDb();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpDelete]
+        [ODataRoute("Persons({id})")]
+        public IHttpActionResult Delete([FromODataUri] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PersonEntity existingPersonEntity = _personRepository.GetSingle(id).First();
+
+            if (existingPersonEntity == null)
+            {
+                return NotFound();
+            }
+
+            _personRepository.Delete(id);
+            _personRepository.SaveToDb();
+
+            return StatusCode(HttpStatusCode.NoContent);
         }
     }
 }
