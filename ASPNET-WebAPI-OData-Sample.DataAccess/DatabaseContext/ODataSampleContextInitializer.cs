6bb211bef54745eb9ca57db18a10b280b9812609
Fabian Gosebrink <fabian.gosebrink@hotmail.com>
2016-06-17 10:54:41 +0200

    small changes in seed

--- ASPNET-WebAPI-OData-Sample.DataAccess/DatabaseContext/ODataSampleContextInitializer.cs
+++ ASPNET-WebAPI-OData-Sample.DataAccess/DatabaseContext/ODataSampleContextInitializer.cs
@@ -10,14 +10,17 @@ namespace ASPNET_WebAPI_OData_Sample.DataAccess.DatabaseContext
 {
     public class ODataSampleContextInitializer : DropCreateDatabaseAlways<ODataSampleContext>
     {
-        //TODO Override Seed here
         protected override void Seed(ODataSampleContext context)
         {
-            context.Houses.Add(new HouseEntity() { City = "Town1", Id = 1, Street = "Street1", ZipCode = 1234,Persons = new List<PersonEntity>()
+            context.Houses.Add(new HouseEntity() { City = "Town1", Id = 1, Street = "Street1", ZipCode = 1234, Persons = new List<PersonEntity>()
             {
                 new PersonEntity() {Age = 30, Prename = "Fabian", Surname = "Gosebrink"}
             }});
-            context.Houses.Add(new HouseEntity() { City = "Town2", Id = 2, Street = "Street2", ZipCode = 1234 });
+            context.Houses.Add(new HouseEntity() { City = "Town2", Id = 2, Street = "Street2", ZipCode = 1234, Persons = new List<PersonEntity>()
+            {
+                new PersonEntity() {Age = 30, Prename = "John", Surname = "Doe"}
+            }
+            });
             context.Houses.Add(new HouseEntity() { City = "Town3", Id = 3, Street = "Street3", ZipCode = 1234 });
             context.Houses.Add(new HouseEntity() { City = "Town4", Id = 4, Street = "Street4", ZipCode = 1234 });
 
